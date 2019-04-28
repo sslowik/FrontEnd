15f1fab427578343162506b925ac719f1f09245a
Sławomir Słowik <sslowik@INF.UG.EDU.PL>
2019-04-28 09:43:50 +0200

    trochę mi brakuje podstaw ziom

---
+++ lab6/reactapp/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function Message() {
+  return <h2>Hello World!!!</h2>
+}
+
+ReactDOM.render(<Message />, document.getElementById('root'));
---
+++ lab7/Index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import React, { Component } from 'react';
+
+class Clock extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.nazwa = props.name;
+        this.state = {
+            date: new Date(),
+            zone: 'GMT'
+        }
+    }
+
+    componentDidMount() {
+        this.timerId = setInterval ( handler: () => {
+            this.tick()
+        }, timeout: 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
+        
+    tick() {
+        this.setState( state; {
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.nazwa}</h1>
+            </div>
+        )
+    }
+
+}

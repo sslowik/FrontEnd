b70f522881f3737fe682ed3ccf8d02c2335abb01
@slawomir.slowik <slawomirslowik@yahoo.pl>
2019-05-12 19:48:48 +0200

    updating CRUD requests

--- lab8/reactserver/index.js
+++ lab8/reactserver/index.js
@@ -3,11 +3,19 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const port = 4000;
-
-app.use(bodyParser.json());
 const numbers = [];
 
-app.get('/api/', (req, res) => res.send('Hi there'));
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to application 'Pacjenci' :) "});
+});
+
+// listen for requests
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.get('/api/', (req, res) => res.send('Hi there! API operational.'));
 
 app.get('/api/numbers/all', (req, res) => {
     res.send(numbers);
@@ -16,20 +24,26 @@ app.get('/api/numbers/all', (req, res) => {
 app.post('/api/numbers', (req, res) => {
     const number = req.body.number;
     numbers.push(number);
-    res.send({"message": "number was added"});
+    res.send({"message": `number ${number} added to database. \n Current array elements: ${numbers}.`});
 });
 
-app.delete('/api/numbers/remove', (req, res) => {
+app.delete('/api/numbers/all', (req, res) => {
     const number = req.body.number;
     
     var i = numbers.indexOf(number);
     if (i > -1) {
         numbers.splice(i, 1)
     }
-    res.send({"message": `number ${number} was deleted`});
+    res.send({"message": `number ${number} deleted. \n Current array elements: ${numbers}.`});
 });
 
-
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.put('/api/numbers/all', (req, res) => {
+    const number = req.body.number;
+    const replacement = req.body.replace;
+    
+    var i = numbers.indexOf(number);
+    if (i > -1) {
+        numbers[i] = replacement
+    }
+    res.send({"message": `Number ${number} with index ${i} replaced with ${replacement}.\n Current array elements: ${numbers}.`});
+});
